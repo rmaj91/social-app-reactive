6f5682d8bd2db29a27956d1b34c2ad368281c8aa
Rafal Maj <rmaj91@gmail.com>
2021-03-16 18:15:56 +0100

    init

---
+++ src/main/java/com/rmaj91/socialappreactive/SocialAppReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.rmaj91.socialappreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAppReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialAppReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmaj91/socialappreactive/SocialAppReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmaj91.socialappreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAppReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
